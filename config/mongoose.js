8bec8f52419b575da7a56844bc70db63ee403e75
Majaz <majazulhaque123@gmail.com>
2023-02-14 12:32:38 +0530

    need  to swith branch to find error

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/codeial_development');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error while connecting to db'));
+
+db.once('open', function(err){
+    if(err){console.log('Error while opening the db'); return;}
+
+    console.log('Connected to the db :: mongoDB');
+})
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,27 @@ module.exports.profile = function(req,res){
         title : 'profile',
     });
 };
+
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: 'Sign Up',
+    });
+};
+
+
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title : 'Sign In',
+    });
+}
+
+module.exports.create = function(req,res){
+    return res.render('user_profile');
+}
+
+
+
+// sign in and create a session for the user
+module.exports.createSession = function(req,res){
+    return res.redirect('/');
+}
